c38792f4ac1425deb6159bba548fdcafc84be85a
CatBox <damian.bonggi@gmail.com>
2019-04-13 23:15:54 +0900

    add memo test
    add memo test

--- scripts/requested-item-table.js
+++ scripts/requested-item-table.js
@@ -23,8 +23,8 @@ function build_requested_item_table(requested_items_map)
                 table_html += "<tr>";
                 for(var i=0; i < item_counter; i++)
                 {
-                    table_html += "<th class =\"note\">";
-                    table_html += "<input type=\"text\" id=\"memo\">";
+                    table_html += "<th class =\"item-amt\">";
+                    table_html += "<input class=\"item-input notranslate\" type=\"number\" min=\"0\" max=\"99\" value=\"0\">";
                     table_html += "</th>";
                 }
                 table_html += "</tr>";
